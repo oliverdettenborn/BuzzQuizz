a50e62dc010237a058c0f8e9dd207c17e62993d2
thaliadettenborn <thalia.born@gmail.com>
2020-09-23 19:17:18 -0300

    implementacao funções para pegar valor de um input, remover espaços no inicio ou fim e tornar primeira letra maiuscula

--- assets/js/funcoesGerais.js
+++ assets/js/funcoesGerais.js
@@ -4,3 +4,36 @@ function trocarDeTela(telaAtual,proximaTela){
     var habilitarTela = document.querySelector(proximaTela);
     habilitarTela.style.display = "block";
 }
+
+function pegarValorInput(identificador){
+    var input = document.querySelector(identificador);
+    var textoInput = input.value;
+    
+    if(textoInput !== ''){
+        input.value = '';
+        return textoInput;
+    }else{
+        input.style.border = "1px solid red";
+        return "vazio"
+    }
+}
+
+function verificarPerguntas(pergunta){
+
+}
+
+function removerEspaçosInicioFim(string){
+    return string.trim();
+}
+
+function primeiraLetraEmMaiusculo(string){
+    var texto = string[0].toUpperCase();
+    texto += string.slice(1)
+    return texto;
+}
+
+function transformarParaClasse(i){
+    if(i === 1) return ".um";
+    else if(i === 2) return '.dois';
+    else if(i === 3) return '.tres';
+}
